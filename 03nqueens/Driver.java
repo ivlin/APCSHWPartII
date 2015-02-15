f425852b16c4fe7c79adfd8d1386f2b2540c3b96
ivan <ivlin112@gmail.com>
2015-02-15 16:28:22 -0500

    Tried another way to solve

--- 03nqueens/Driver.java
+++ 03nqueens/Driver.java
@@ -1,7 +1,9 @@
+import java.io.*;
 public class Driver{
 
     public static void main(String[]args){
 	NQueens n = new NQueens(Integer.parseInt(args[0]));
+
 	if (n.solve()){
 	    System.out.println(n);
 	}else{
--- 03nqueens/NQueens.java
+++ 03nqueens/NQueens.java
@@ -1,15 +1,18 @@
+import java.util.Arrays;
+import java.io.*;
 public class NQueens{
     //constants for the class
     //terminal specific character to clear screen , or hide/show cursor
     final static String clear =  "\033[2J";
     final static String hide =  "\033[?25l";
     final static String show =  "\033[?25h";
-
+    int cnt = 0;
     //instance variable
     private char[][]board;
 
+
     private void clearTerminal(){
-	System.out.println("clear");
+	System.out.println(clear);
     }
 
     //terminal specific character to move the cursor
@@ -27,6 +30,11 @@ public class NQueens{
 
     public NQueens(int n){
 	board = new char[n][n];
+	for (int i = 0; i < n; i++){
+	    for (int t = 0; t < n; t++){
+		board[i][t] = '.';
+	    }
+	}
     }
 
     public String toString(){
@@ -41,27 +49,47 @@ public class NQueens{
     }
     
     public boolean solve(){
-	int [] col = new int[board.length];
-	for (int c = 0; c < col.length; c++){
-	    col[c] = -1;
+	int[]current = new int[board.length];
+	for (int i = 0; i < current.length; i++){
+	    current[i] = -9;
 	}
-	solve(0, 0, 0, col);			
+	return solve(0, 1, 0);
     }
 
-    public boolean solve(int x, int y, int qcount, int[]col){
+    public boolean solve(int row, int col, int qcount){
+	cnt++;
 	System.out.println(this);
-wait(10);
-	if (qcount == board.length + 1){
+	wait(50);
+	System.out.println(cnt);
+	if (row == board.length - 1){
 	    return true;
-	}
-	if (x < 0 || x >= board.length || y < 0 || y >= board.length){
+	}	
+	if (row < 0 || row >= board.length || col < 0 || col >= board.length){	
 	    return false;
 	}
-      	for (int i = 0; i < col.length; i++){
-	    if (y - x == col[i] - i || y + board.length - x == col[i] + col.length - i){
-		return false;
+	int temp;
+	if (row < board.length - 1){
+	    for (int i = 0; i < board.length; i++){
+		temp = Character.getNumericValue(board[row][i]);
+		if (temp >= 0 && col - 1 + temp >= 0 && col - 1 + temp < board.length){
+		    board[row + 1][col - 1 + temp] = board[row][col];
+		}
 	    }
 	}
-
+	if (board[row][col] == '.'){	 
+	    board[row][col] = 'Q';
+	    if (col > 0){
+		board[row + 1][col - 1] = '0';
+	    }
+	    if (col < board.length - 1){
+		board[row + 1][col + 1] = '2';
+	    }
+	    board[row + 1][col] = '1';
+	    if (solve(row + 1, 0, qcount + 1)){
+		return true;
+	    }
+	    board[row][col] = '.';
+	}
+	return solve(row, col + 1, qcount);
     }
 }
