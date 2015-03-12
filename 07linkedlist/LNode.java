3ba8667dbf4350b693984b00d1d31228bf2367b0
Ivan Lin <ivlin112@gmail.com>
2015-03-12 12:24:48 -0400

    A

--- 07linkedlist/LNode.java
+++ 07linkedlist/LNode.java
@@ -1,18 +1,34 @@
 public class LNode{
 
-    private Object data;
+    private int data;
     private LNode next;
 
-    public LNode(Object newData, LNode newNext){
+    public LNode(int newData){
+	this(newData, null);
+    }
+
+    public LNode(int newData, LNode newNext){
 	setData(newData);
 	setNext(newNext);	
     }
 
-    public void setData(Object newData){
+    public void setData(int newData){
 	data = newData;
+    }
+
+    public int getData(){
+	return data;
     }
 
     public void setNext(LNode newNext){
 	next = newNext;
+    }
+
+    public LNode getNext(){
+	return next;
+    }
+
+    public String toString(){
+	return "" + data;
     }
 }
