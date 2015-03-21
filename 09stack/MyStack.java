2736335cc62dcd5fa42237a446fd2e72248541f8
ivan <ivlin112@gmail.com>
2015-03-21 17:59:39 -0400

    Wrote all methods using arraylist

---
+++ 09stack/MyStack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class MyStack<T>{
+    private ArrayList<T> stack;
+
+    public MyStack(){
+	stack = new ArrayList<T>();
+
+    }
+
+    public T push(T value){
+	stack.add(value);
+	return value;
+    }
+
+    public T peek(){
+	return stack.get(stack.size() - 1);
+    }
+
+    public T pop(){
+	return stack.remove(stack.size() - 1);
+    }
+
+    public boolean isEmpty(){
+	return size() == 0;
+    }
+
+}
