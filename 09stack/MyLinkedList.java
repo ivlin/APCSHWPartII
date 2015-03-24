18525e262e76026f5e6d9d4607a9e71e25215419
Ivan Lin <ivlin112@gmail.com>
2015-03-24 12:24:35 -0400

    added mll and nl

---
+++ 09stack/MyLinkedList.java
@@ -0,0 +1,148 @@
+import java.util.*;
+public class MyLinkedList<T> implements Iterable<T>{
+
+    private class MyLLIterator<T> implements Iterator<T>{
+	private LNode<T> now;
+
+	public MyLLIterator(LNode<T> first){
+	    now = first;
+	}
+
+	public boolean hasNext(){
+	    return now.getNext() != null; 
+	}
+
+	public T next(){
+	    T data = now.getData();
+	    now = now.getNext();
+	    return data;
+	}
+
+	public void remove() throws UnsupportedOperationException{
+	    throw new UnsupportedOperationException();
+	}
+
+    }
+
+    private LNode<T> first, last, current;
+    private int size;
+
+    public MyLinkedList(){
+	size = 0;
+    }
+
+    public Iterator<T> iterator(){
+	return new MyLLIterator<T>(first);
+    }
+
+    public String name(){
+	return "lin.ivan";
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public int indexOf(T val){
+	current = first;
+	int ind = 0;
+	while (current.getNext() != null){
+	    if (current.getData() == val){
+		return ind;
+	    }else{
+		current = current.getNext();
+	    }
+	}
+	return -1;
+    }
+
+    public T get(int ind) throws IndexOutOfBoundsException{
+	if (ind >= size() || ind < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	current = first;
+	for (int i = 0; i < ind; i++){
+	    current = current.getNext();
+	}
+	return current.getData();
+    }
+
+    public boolean add(T val) throws IndexOutOfBoundsException{
+	return add(size(), val);
+    }
+
+    public boolean add(int ind, T val) throws IndexOutOfBoundsException{
+	if (ind > size() || ind < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	if (size == 0){
+	    first = last = new LNode<T>(val);
+	}else if (ind == size()){
+	    last.setNext(new LNode<T>(val));
+	    last = last.getNext();
+	}else{
+	    current = first;
+	    for (int i = 0; i < ind - 1; i++){
+		current = current.getNext();
+	    }
+	    current.setNext(new LNode<T>(val, current.getNext()));
+	}	
+	size++;
+	return true;
+    }
+
+    public T set(int ind, T val) throws IndexOutOfBoundsException{
+	if (ind >= size() || ind < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	if(ind == size){
+	    last.setData(val);
+	}
+	current = first;
+	for (int i = 0; i < ind; i++){
+	    current = current.getNext();
+	}
+	T data = current.getData();
+	current.setData(val);
+	return data;
+    }
+
+    public boolean remove(int ind) throws IndexOutOfBoundsException{
+	if (ind >= size() || ind < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	current = first;
+	if (ind == 0){
+	    first = first.getNext();
+	}
+	if (size == 0){
+	    first = null;
+	}
+	for (int i = 0; i < ind - 1; i++){
+	    current = current.getNext();
+	}
+	//int temp = current.getNext().getData();
+	if (ind < size - 1){
+	    current.setNext(current.getNext().getNext());
+	}else{
+	    last = current;
+	    current.setNext(null);
+	}
+	size--;
+	return true;    
+    }
+
+    public String toString(){
+	String str = "[";
+	current = first;
+	while (current != null){
+	    str += current.getData() + ",";
+	    current = current.getNext();
+	}
+	return str.substring(0, str.length() - 1) + "]";
+    }
+
+    public boolean isEmpty(){
+	return size == 0;
+    }
+}
