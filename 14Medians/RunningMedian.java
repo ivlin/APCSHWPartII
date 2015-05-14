bcdd6e54fbebfc5f760c21f93b8b461af9401e2b
Ivan Lin <ivlin112@gmail.com>
2015-05-14 12:20:30 -0400

    Doesnt work

--- 14Medians/RunningMedian.java
+++ 14Medians/RunningMedian.java
@@ -1,6 +1,6 @@
 public class RunningMedian{
     
-    private MyHeap first, last;
+    public MyHeap first, last;
 
     public RunningMedian(){
 	first = new MyHeap(true);
@@ -10,6 +10,9 @@ public class RunningMedian{
     public void add(int val){
 	if (first.getSize() == last.getSize()){
 	    first.add(val);
+	}
+	else if (last.getSize() == 0){
+	    last.add(val);
 	}
 	else if (first.getSize() != last.getSize()){
 	    last.add(first.remove());
@@ -21,5 +24,14 @@ public class RunningMedian{
 	    return (double)(first.peek() + last.peek()) / 2.0; 
 	}
 	return (double)(first.peek());
+    }
+
+    public static void main(String[]args){
+	RunningMedian m = new RunningMedian();
+	for (int i = 0; i < 20; i++){
+	    m.add((int)(Math.random() * 100));
+	}
+	System.out.println(m.first + " " + m.last);
+	System.out.println(m.getMedian());
     }
 }
